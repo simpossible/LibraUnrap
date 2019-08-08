74d68546d64efd58aecede69215571ab7c057646
simpossible <ljfsteven@163.com>
2019-08-08 23:21:02 +0800

    first commint

---
+++ LibraUnWraps/mnemonic/Mnemonic.h
@@ -0,0 +1,19 @@
+//
+//  Mnemonic.h
+//  LibraUnWraps
+//
+//  Created by simp on 2019/8/8.
+//  Copyright © 2019 simp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Mnemonic : NSObject
+
+- (NSString *)generated;
+
+@end
+
+NS_ASSUME_NONNULL_END
